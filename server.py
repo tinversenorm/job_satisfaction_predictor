35db0c5c81b1ecf315fc66b0bf8df125d7f58a78
Pranav Harathi <s.pranav.harathi@gmail.com>
2018-05-02 23:58:25 -0500

    Starting adding questions

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 Simple website to present questions, format them and then run the model.
 Run with `FLASK_APP=server.py flask run` after doing a pip install.
 """
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from models.predict import predict
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = "super secret key"
@@ -15,7 +15,8 @@ def upload():
     return render_template('questions.html')
 
 @app.route('/predict')
-def predict_job_satisfaction(data=None):
+def predict_job_satisfaction():
+    data = request.args
     if data is None:
         return jsonify({"error": "No Data Provided"})
     else:
